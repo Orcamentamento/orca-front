08d420254103e46925735d1b0191cbf838d513ec
raulvenson <raulvenson@gmail.com>
2024-03-04 08:28:43 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuLateralComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TabMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu-lateral',
+  templateUrl: './menu-lateral.component.html',
+  styleUrls: ['./menu-lateral.component.css']
+})
+export class MenuLateralComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  activeItem: MenuItem | undefined;
+
+  ngOnInit() {
+      this.items = [
+          { label: 'Home', icon: 'pi pi-fw pi-home' },
+          { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+          { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+          { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+          { label: 'Settings', icon: 'pi pi-fw pi-cog' }
+      ];
+
+      this.activeItem = this.items[0];
+  }
+}
